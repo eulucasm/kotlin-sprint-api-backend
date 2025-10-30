f7af1ea8ed3f28983d4978536980beacfc7faf0f
Lucas Marques <lucasmarques@ciandt.com>
2025-10-30 14:41:40 -0300

    add projeto ao repo

---
+++ src/main/kotlin/br/com/mercadolivro/controller/CustomerController.kt
@@ -0,0 +1,29 @@
+package br.com.mercadolivro.controller
+
+import br.com.mercadolivro.controller.request.PostCustomerRequest
+import br.com.mercadolivro.model.CustomerModel
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.*
+
+@RestController
+@RequestMapping("customers")
+class CustomerController {
+
+    val customers = mutableListOf<CustomerModel>()
+
+    @GetMapping
+    fun getCustomer(): List<CustomerModel> {
+        return customers
+    }
+
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    fun createCustomer(@RequestBody customer: PostCustomerRequest) {
+        val id = if (customers.isEmpty()) {
+            "1"
+        } else {
+            customers.last().id.toInt() + 1
+        }.toString()
+        customers.add(CustomerModel(id, customer.name, customer.email))
+    }
+}
